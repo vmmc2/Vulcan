7c5da131a4a74368ce105ed4c507c518de05fc80
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-05-15 16:30:07 -0700

    Update simulator.dart

--- vulcan/lib/screens/simulator.dart
+++ vulcan/lib/screens/simulator.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vulcan/components/responsive_widget.dart';
 import 'package:vulcan/components/top_bar.dart';
 import 'package:vulcan/components/action_bar.dart';
 import 'package:vulcan/screens/error.dart';
+
+List<String> teste = ['Teste', 'Para', 'Ver', 'Como', 'O', 'List', 'View', 'Funciona', 'Em', 'Detalhes','porra', 'caralho', 'misera','0','1','2','3','4','5','6','7','8','9'];
+
 
 class Simulator extends StatefulWidget {
   @override
@@ -29,6 +33,128 @@ class _SimulatorState extends State<Simulator> {
                     fontSize: 15.0,
                     color: Color(0xFF1CFDFC),
                   ),
+                ),
+                SizedBox(height: 20.0),
+                Row( //Nessa linha eu vou ter o status do simulador com as instrucoes executadas e a sua direita eu vou ter os registradores e a cache
+                  children: <Widget>[
+                    SizedBox(width: 20.0),
+                    Column( //Primeira coluna, vai ficar o simulador.
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Simulator',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 20.0,
+                            color: Color(0xFF1CFDFC),
+                          ),
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Color(0xFF8EEDF7),
+                            ),
+                            height: 500.0,
+                            width: 980.0,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: Text(
+                                        'PC',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontFamily: 'Poppins',
+                                          fontSize: 12.0,
+                                          color: Color(0xFF02143D),
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      flex: 4,
+                                      child: Text(
+                                        'Machine Code',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontFamily: 'Poppins',
+                                          fontSize: 12.0,
+                                          color: Color(0xFF02143D),
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Text(
+                                        'Original Code',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontFamily: 'Poppins',
+                                          fontSize: 12.0,
+                                          color: Color(0xFF02143D),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Expanded( //Por algum motivo, para usar ListView ou ListView.builder dentro de um widget sem ser Scaffold tem que por ela dentro de um Expanded() antes.
+                                  child: ListView.builder(
+                                    padding: const EdgeInsets.all(8.0),
+                                    itemCount: teste.length,
+                                    itemBuilder: (BuildContext context, int index){
+                                      return Container(
+                                        margin: EdgeInsets.all(3.0),
+                                        height: 50.0,
+                                        decoration: BoxDecoration(
+                                          color: Colors.red,
+                                          borderRadius: BorderRadius.circular(8.0),
+                                        ),
+                                        child: Text(
+                                          '${teste[index]}',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontFamily: 'Poppins',
+                                            fontSize: 14.0,
+                                          ),
+                                        ),
+                                      );
+                                    }
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 30.0),
+                    Column( //segunda coluna vai ficar os registradores e a memoria.
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'Status',
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 20.0,
+                              color: Color(0xFF1CFDFC),
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(15.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Color(0xFF8EEDF7),
+                              ),
+                              height: 500.0,
+                              width: 500.0,
+                            ),
+                          ),
+                        ],
+                    ),
+                  ],
                 ),
               ],
             ),
