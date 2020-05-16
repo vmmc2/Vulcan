ed6172d081b05c3d776c77f5a1abea74c2572630
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-05-15 17:30:38 -0700

    Create instruction_card.dart

---
+++ vulcan/lib/utilities/instruction_card.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class InstructionCard{
+  final String pc;
+  final String machineCode;
+  final String originalCode;
+
+  InstructionCard({@required this.pc, @required this.machineCode, @required this.originalCode});
+}
