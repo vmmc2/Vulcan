f97f651634d119d2b2dca2dc81568b46c08afb89
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-11 14:39:58 -0700

    Update floating_register_list.dart

--- vulcan/lib/components/floating_register_list.dart
+++ vulcan/lib/components/floating_register_list.dart
@@ -1,3 +1,6 @@
+// Vulcan is Software developed by:
+// Victor Miguel de Morais Costa
+// License: MIT
 import 'package:flutter/material.dart';
 import 'package:vulcan/components/register_card.dart';
 
