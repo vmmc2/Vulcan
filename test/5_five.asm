217babb7f56e2e619a5140ec0e65a9a5ef704ad4
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-28 14:47:48 -0700

    Update 5_five.asm

--- test/5_five.asm
+++ test/5_five.asm
@@ -73,7 +73,9 @@ sb x8, 0, x5
 addi x5, x5, 1
 
 # pontuacao do jogador 1  -->  registrador x9
+addi x9, x0, 0
 # pontuacao do jogador 2 --> registrador x10
+addi x10, x0, 0
 
 #checando a pontuacao do jogador1/string1
 loop:
@@ -107,13 +109,66 @@ loop:
   addi x11, x0, 66  # 'B'
   beq x8, x11, soma3
   addi x11, x0, 67  # 'C'
+  beq x8, x11, soma3
+  addi x11, x0, 77  # 'M'
+  beq x8, x11, soma3
+  addi x11, x0, 80  # 'P'
+  beq x8, x11, soma3
 
+  addi x11, x0, 70  # 'F'
+  beq x8, x11, soma4
+  addi x11, x0, 72  # 'H'
+  beq x8, x11, soma4
+  addi x11, x0, 86  # 'V'
+  beq x8, x11, soma4
+  addi x11, x0, 87  # 'W'
+  beq x8, x11, soma4
+  addi x11, x0, 89  # 'Y'
+  beq x8, x11, soma4
+
+ addi x11, x0, 75  # 'K'
+ beq x8, x11, soma5
+
+ addi x11, x0, 74  # 'J'
+ beq x8, x11, soma8
+ addi x11, x0, 76  # 'L'
+ beq x8, x11, soma8
+ addi x11, x0, 88  # 'X'
+ beq x8, x11, soma8
+
+ addi x11, x0, 81  # 'Q'
+ beq x8, x11, soma10
+ addi x11, x0, 90  # 'Z'
+ beq x8, x11, soma10
 
 soma1:
-
+  addi x9, x9, 1
+  addi x6, x6, 1
+  jal x0, loop
 soma2:
-
-
+  addi x9, x9, 2
+  addi x6, x6, 1
+  jal x0, loop
+soma3:
+  addi x9, x9, 3
+  addi x6, x6, 1
+  jal x0, loop
+soma4:
+  addi x9, x9, 4
+  addi x6, x6, 1
+  jal x0, loop
+soma5:
+  addi x9, x9, 5
+  addi x6, x6, 1
+  jal x0, loop
+soma8:
+  addi x9, x9, 8
+  addi x6, x6, 1
+  jal x0, loop
+soma10:
+  addi x9, x9, 10
+  addi x6, x6, 1
+  jal x0, loop
 
 
 fim1:
