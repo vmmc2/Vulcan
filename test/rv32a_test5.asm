596afa04c7be5272de1c38dc48210564d7835271
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-08-21 14:28:00 -0700

    Create rv32a_test5.asm

---
+++ test/rv32a_test5.asm
@@ -0,0 +1,9 @@
+addi x10, x0, 10
+addi x11, x0, -2
+addi x12, x0, 34
+addi x13, x0, 37
+addi x14, x0, 4
+sw x10, 0, x0  # memoria[0..3] = 10
+sw x13, 4, x0  # memoria[4..7] = 37
+amomin.w x20, x11, x0
+amomin.w x21, x12, x14
