16fbcae435ef786fd7f964a7e7960d1d531683a5
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-09-23 09:29:54 -0300

    Create rv32f_test3.asm

---
+++ test/rv32f_test3.asm
@@ -0,0 +1,12 @@
+addi x10, x0, 10
+addi x11, x0, 11
+addi x12, x0, 9
+addi x13, x0, 2
+fcvt.s.w f0, x10
+fcvt.s.w f1, x11
+fcvt.s.w f2, x12
+fcvt.s.w, f3, x13
+fdiv.s f4, f2, f3
+fsqrt.s f5, f4
+fsub.s f6, f4, f3
+fmadd.s f7, f4, f3, f5
